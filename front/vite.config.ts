e985454f70c98b50c3555a97bf4509021f7324d3
Raph <raph0222@naver.com>
2024-09-25 00:50:49 +0900

    [fix] Adjustments :
    - jenkins files cleaner : branch/repo parameter, cleaner format checking before build, changing path after refactoring
    - cleaner format checking : eslint/prettier
    - using package.json instead of .env to handle version
    .env added to .gitignore => .env.example instead

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { version } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  define: {
+    'import.meta.env.VERSION': JSON.stringify(version)
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
