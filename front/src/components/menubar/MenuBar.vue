b7d296575dc193f21e66750ec6ce4afe110af62d
Raph <raph0222@naver.com>
2024-09-26 16:29:28 +0900

    Front adjustment : Mobile nav bar at the bottom

--- front/src/components/menubar/MenuBar.vue
+++ front/src/components/menubar/MenuBar.vue
@@ -15,25 +15,52 @@
 
   <!-- mobile -->
   <div class="flex lg:hidden fixed bottom-0 left-0 right-0 bg-black text-white z-50">
-    <nav class="w-full">
-      <ul class="flex justify-around py-2">
+    <nav class="w-full flex">
+      <ul v-show="isMenuVisible" class="flex justify-around py-2">
         <li><TitleMenuBar title="Contact" sectionRef="#contact-section" /></li>
         <li><TitleMenuBar title="Bio" sectionRef="#details-section" /></li>
         <li><TitleMenuBar title="Experience" sectionRef="#experience-section" /></li>
         <li><TitleMenuBar title="Stack" sectionRef="#stack-section" /></li>
       </ul>
+      <button
+        class="absolute bottom-4 right-4 p-2 bg-white text-black rounded-full"
+        @click="toggleMenu"
+      >
+        &#9776;
+      </button>
     </nav>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import TitleMenuBar from '@/components/menubar/TitleMenuBar.vue'
 
 export default defineComponent({
   name: 'MenuBar',
   components: {
     TitleMenuBar
+  },
+  setup() {
+    const isMenuVisible = ref(false)
+
+    const toggleMenu = () => {
+      isMenuVisible.value = !isMenuVisible.value
+    }
+
+    return {
+      isMenuVisible,
+      toggleMenu
+    }
   }
 })
 </script>
+
+<style scoped>
+button {
+  transition: background-color 0.3s;
+}
+button:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+</style>
