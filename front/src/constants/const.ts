a4acc2d7a6e02e5db9e1f128ed7d7a91355e5d32
Raph <raph0222@naver.com>
2024-10-10 16:05:27 +0900

    Update resume

--- front/src/constants/const.ts
+++ front/src/constants/const.ts
@@ -1,6 +1,6 @@
 const LINKEDIN_PROFILE = 'https://www.linkedin.com/in/raro217/'
 const GITHUB_PROFILE = 'https://github.com/raph0222'
 const MAIL = 'raph0222@naver.com'
-const PDF_URL = '/ROCHA_29-09.pdf'
+const PDF_URL = '/ROCHA_RESUME.pdf'
 
 export default { LINKEDIN_PROFILE, MAIL, GITHUB_PROFILE, PDF_URL }
